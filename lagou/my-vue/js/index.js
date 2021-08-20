1681eddb4c82675a95ca15d6163db5809da61d87
lambortao <lambortao@qq.com>
2021-08-20 18:48:03 +0800

    提交

---
+++ lagou/my-vue/js/index.js
@@ -0,0 +1,15 @@
+class Vue {
+  // 创建 vue 实例 
+  constructor (options) {
+    /**
+     * 1. 通过属性保存选项的数据
+     * 2. 把 data 中的成员转换成 getter 和 setter，然后在注入到 vue 实例上去
+     * 3. 调用 observer 对象，监听数据变化
+     * 4. 调用 compiler 对象，解析指令和差值表达式
+     */
+  }
+
+  _proxyData (data) {
+
+  }
+}
